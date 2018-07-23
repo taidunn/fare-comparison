7d8463fec1af03c6ae9130738cc10ce946b22bcc
Anthon Fiddes <anthonyfiddes@hotmail.com>
2018-07-23 11:58:19 -0400

    Updated .gitignore and changed variables to snake case

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import jinja2
 import lyft_rides
 from uber_rides.session import Session
 from uber_rides.client import UberRidesClient
+import uber
 
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,6 +14,8 @@ jinja_env = jinja2.Environment(
 
 class GreetingsPage(webapp2.RequestHandler):
     def get(self):
+        session = Session(server_token=uber.server_token)
+        client = UberRidesClient(session)
         home_template = jinja_env.get_template("templates/index.html")
         self.response.write(home_template.render())# Home Page
 
