29be1c1163b129ea84a84a9e470898a60a9c7232
Anthon Fiddes <anthonyfiddes@hotmail.com>
2018-07-23 11:25:41 -0400

    Revert "Updated .gitignore"
    This reverts commit cba9d5e0097c969fe7fb9b481be0bccc2a39cf45.

--- main.py
+++ main.py
@@ -2,9 +2,6 @@
 import webapp2
 import os
 import jinja2
-import lyft_rides
-from uber_rides.session import Session
-from uber_rides.client import UberRidesClient
 
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
