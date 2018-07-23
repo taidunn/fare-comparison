47ce45277e3c81fc3dac2e683f215ce27ea62b0d
Tai Dunn <tai.dunn.ahs@gmail.com>
2018-07-23 10:16:13 -0400

    New File
    New File Database File

---
+++ data.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import ndb
+import time
+class BankData(ndb.Model):
+ longitude = ndb.StringProperty(required=True)
+ latitude = ndb.StringProperty(required=True)
+ address = ndb.StringProperty(required=True)
+ timestamp = ndb.DateTimeProperty(required=True, auto_now_add=True)
