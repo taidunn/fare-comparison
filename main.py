b58569550e842633d9a79519dbd95fec5fed5cfa
Anthon Fiddes <anthonyfiddes@hotmail.com>
2018-07-23 14:43:52 -0400

    Basic uber code added
    Does not work yet, but is not breaking the server.

--- main.py
+++ main.py
@@ -12,10 +12,23 @@ jinja_env = jinja2.Environment(
     autoescape = True,
 )
 
+def get_uber_estimate():
+    # This is currently giving a 500 error, but should be working
+    # Ultimately, coordinates would be passed into this function to get an estimate, perhaps
+    uber_session = Session(server_token=uber.server_token)
+    uber_client = UberRidesClient(uber_session)
+    uber_response = uber_client.get_price_estimates(
+        start_latitude=37.770,
+        start_longitude=-122.411,
+        end_latitude=37.791,
+        end_longitude=-122.405,
+        seat_count=2
+    )
+    estimate = uber_response.json.get('prices')
+    return estimate
+
 class GreetingsPage(webapp2.RequestHandler):
     def get(self):
-        session = Session(server_token=uber.server_token)
-        client = UberRidesClient(session)
         home_template = jinja_env.get_template("templates/index.html")
         self.response.write(home_template.render())# Home Page
 
