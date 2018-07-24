9523821e58c0dc7a0130b2bd2140b28d345dd1f8
Anthon Fiddes <anthonyfiddes@hotmail.com>
2018-07-24 09:11:17 -0400

    Restoring some previous changes

--- main.py
+++ main.py
@@ -3,13 +3,46 @@ import webapp2
 import os
 import jinja2
 import lyft_rides
-from uber_rides.session import Session
+import uber
+from uber_rides.session import Session as uSession
 from uber_rides.client import UberRidesClient
+import lyft
+from lyft_rides.auth import ClientCredentialGrant
+from lyft_rides.session import Session as lSession
+from lyft_rides.client import LyftRidesClient
 
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
     autoescape = True,
 )
+
+def get_uber_estimate():
+    # This is currently giving a 500 error, but should be working
+    # Ultimately, coordinates would be passed into this function to get an estimate, perhaps
+    uber_session = uSession(server_token=uber.server_token)
+    uber_client = UberRidesClient(uber_session)
+    uber_response = uber_client.get_price_estimates(
+        start_latitude=37.770,
+        start_longitude=-122.411,
+        end_latitude=37.791,
+        end_longitude=-122.405,
+        seat_count=2
+    )
+    estimate = uber_response.json.get('prices')
+    return estimate
+
+def get_lyft_estimate():
+    auth_flow = ClientCredentialGrant(
+        lyft.client_id,
+        lyft.client_secret,
+        ['public']
+    )
+
+    session = auth_flow.get_session()
+    client = LyftRidesClient(session)
+    response = client.get_ride_types(37.7833, -122.4167)
+    ride_types = response.json.get('ride_types')
+    return ride_types
 
 class GreetingsPage(webapp2.RequestHandler):
     def get(self):
