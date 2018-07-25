5bf1fa5f3725e1e7e39682e07f54b4d653397d60
Anthon Fiddes <anthonyfiddes@hotmail.com>
2018-07-25 15:50:17 -0400

    api mostly working
    A commit to get us started. All prices can be gathered easily, and finding the eta for a lyft ride works. I only have the Uber ETAs left to do.

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -1,4 +1,6 @@
-const searchBoxes = document.getElementsByClassName("search_box");
-for (input of searchBoxes){
-  autocomplete = new google.maps.places.Autocomplete(input, {});
-};
+const options = {country: "us"};
+const fromAddress = document.getElementById("from_address");
+const acFromAddress = new google.maps.places.Autocomplete(fromAddress, options);
+const toAddress = document.getElementById("to_address");
+const acToAddress = new google.maps.places.Autocomplete(toAddress, options);
+const address_form = document.getElementById("address_form");
