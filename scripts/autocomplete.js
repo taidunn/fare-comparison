67a96a1e339a549f200cc7595c309e45967f0ee4
Anthon Fiddes <anthonyfiddes@hotmail.com>
2018-07-26 10:11:36 -0400

    IT WORKS!

--- scripts/autocomplete.js
+++ scripts/autocomplete.js
@@ -4,3 +4,10 @@ const acFromAddress = new google.maps.places.Autocomplete(fromAddress, options);
 const toAddress = document.getElementById("to_address");
 const acToAddress = new google.maps.places.Autocomplete(toAddress, options);
 const address_form = document.getElementById("address_form");
+
+const form = document.getElementById("address_form");
+form.addEventListener("keypress", function(e){
+  if(e.keyCode == 13){
+    e.preventDefault()
+  };
+});
