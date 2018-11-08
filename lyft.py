2863e0860ff481bc7e0f8b3f1cf9e3669c49b224
Tai <macuser@Macs-MacBook-Pro-6.local>
2018-11-08 15:33:34 -0500

    Update lyft.py

--- lyft.py
+++ lyft.py
@@ -1,3 +1,3 @@
-client_id = "OheoVsyE66AW"
+client_id = "CP2zM5SwUJJ6"
 client_secret = "o_TOMfaeFKSUWhAu-6kjE-ytFs-MK9th"
 server_token = "zrfPATaJAyuIzGdz46Rc/XpupKQx8FZzG5P9H81mj9t7VfOKhBQzDPjKjhNPSjsJaFebIuUcQBoNf99JwUqnfZvqfjXJoW6c6Kvi/zGR13i7DiXJI0VwYX8="
